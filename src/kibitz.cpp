f3d459d101d5141abae8905e0df508233f3624dc
John Murphy <murphybytes@gmail.com>
2012-08-09 22:51:43 -0500

    started to flesh out the interface

--- src/kibitz.cpp
+++ src/kibitz.cpp
@@ -12,5 +12,17 @@ namespace kibitz {
   context::context() {
     std::cout << "ctor" << std::endl;
   }
+
+  void context::set_worker_type( const string& worker_type_name ) {
+    worker_type_name_ = worker_type_name;
+  }
+
+  void context::set_worker_id( const string& worker_id ) {
+    worker_id_ = worker_id ;
+  }
+
+  void context::register_heartbeat_handler( callback heartbeat_handler ) {
+    heartbeat_handler_ = heartbeat_handler;
+  }
 }
 
--- src/kibitz.hpp
+++ src/kibitz.hpp
@@ -2,13 +2,27 @@
 #define __KIBITZ_HPP__
 
 #include <iostream>
+#include <string>
+
+using std::string;
 
 namespace kibitz {
 
+  typedef void ( *callback )( const string&  );
+
   class context {
     friend context& get_context();
+    
+    string worker_type_name_;
+    string worker_id_;
+    callback heartbeat_handler_;
 
     context();
+  public:
+    void set_worker_type( const string& worker_type_name ) ;
+    void set_worker_id( const string& worker_id );
+    void register_heartbeat_handler( callback heartbeat_handler );
+    
   };
 
   context& get_context() ; 
